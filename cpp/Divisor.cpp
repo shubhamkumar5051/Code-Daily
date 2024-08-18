508c384b9679851a025426b2a9cb69a12b5a3325
shubham <shubhamkr5051@gmail.com>
2024-08-18 23:56:18 +0530

    divisor

--- cpp/Divisor.cpp
+++ cpp/Divisor.cpp
@@ -7,15 +7,43 @@ int main()
 
     //brute force approach
 
-    for(int i=1; i<=num; i++)
-    {
-        if(num%i==0)
-        {
-            cout<<i<<" ";
-        }
-    }
+    // for(int i=1; i<=num; i++)
+    // {
+    //     if(num%i==0)
+    //     {
+    //         cout<<i<<" ";
+    //     }
+    // }
 
 
     //optimal solution
+
+    //divisor of 36
+    /*
+    1*36
+    2*18
+    3*12
+    4*9
+    6*6   ------------- sqrt of 36
+    36*1
+    18*2
+    12*3
+    9*4
+    */
+
+
+    for (int i = 1; i <=sqrt(num); i++)
+    {
+        if(num%i==0)
+        {
+        if(i == num/i)
+        {
+          cout<<i<<endl;
+        }else{
+        cout<<i<<" "<<num/i<<endl;
+        }
+        }
+    }
+    
     return 0;
 }
