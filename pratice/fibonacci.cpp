2d6cd39d82544832cda63a676e161f0eed50b434
shubham <shubhamkr5051@gmail.com>
2024-08-25 19:11:04 +0530

    fibonacci

--- pratice/fibonacci.cpp
+++ pratice/fibonacci.cpp
@@ -2,26 +2,39 @@
 using namespace std;
 
 
-void print_fibonacci(int num)
+// void print_fibonacci(int num)
+// {
+//    //-1 1 0 1 1 2 3 5 8 13
+
+//    int first = -1;
+//    int second = 1;
+//    int third;
+
+//    for(int i=0;i<=num;i++)
+//    {
+//     third = first+second;
+//     cout<<third<<" ";
+//     first = second;
+//     second = third;
+//    }
+// }
+
+
+int print_fibonacci(int num)
 {
    //-1 1 0 1 1 2 3 5 8 13
 
-   int first = -1;
-   int second = 1;
-   int third;
+  if(num == 0)
+  {
+    return 0;
+  }
 
-   for(int i=0;i<=num;i++)
-   {
-    third = first+second;
-    cout<<third<<" ";
-    first = second;
-    second = third;
-   }
+  return print_fibonacci(num-1) + print_fibonacci(num-2);
 }
 
 
 int main()
 {
-    int num=15;
-    print_fibonacci(num);
+    int num=4;
+    cout<<print_fibonacci(num);
 }
