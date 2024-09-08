e8a971c4d8fd8d72366f64337e0c72fd96e02877
shubham <shubhamkr5051@gmail.com>
2024-09-09 01:01:59 +0530

    remove_duplicate

---
+++ pratice/sec_largest_smallest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int arr[]= {1, 2, 3, 4, 5};
+    int count = sizeof(arr)/sizeof(arr[0]);
+    int largest =  INT16_MIN;
+    int sec_largest = ;
+    int smallest;
+    int sec_smallest;
+
+    for (int i = 0; i < count; i++)
+    {
+        
+    }
+    
+    return 0;
+}
